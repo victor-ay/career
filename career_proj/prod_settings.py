8a724c63c675d22dac1ac451611a8b0f4e357370
Victor Aynbinder <>
2023-04-18 11:54:44 +0300

    prod_settings -> looking for bad configuration

--- career_proj/prod_settings.py
+++ career_proj/prod_settings.py
@@ -148,7 +148,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
