2396078b792ae18b635b02210aa931d8b4348997
Victor Aynbinder <>
2023-05-07 15:16:24 +0300

    Adding test file for ec2. Need to delete it after

--- jobs_scrappers/print_hello.py
+++ jobs_scrappers/print_hello.py
@@ -1,1 +1,7 @@
+import json
+
 print("Hello from (print_hello)")
+with open('linkedin_scrapper/scraped_files/israel_test_4_noTitle-2023-05-03.json', 'r') as fh:
+    all_file = json.load(fh)
+
+print(f"File length  = {len(all_file)}")
