b1dda33f57f0d58f377a581d1d7d22a57304eaf7
Victor Aynbinder <>
2023-05-07 17:50:11 +0300

    Adding test file for ec2. Need to delete it after

--- ec2_tests/ec2_test_2.py
+++ ec2_tests/ec2_test_2.py
@@ -1,6 +1,7 @@
 from ec2_test_1 import Ec2_test1
 import ec_subfolder
-from ec2_tests.ec_subfolder.ec2_sub_class import Ec2_test_subclass
+# from ec2_tests.ec2_test_1 import Ec2_test1
+from ec_subfolder.ec2_sub_class import Ec2_test_subclass
 
 my_test = Ec2_test1
 my_test2 = Ec2_test_subclass
