2e3a075b0bc1d2d540a49a2e6bc6fd7fecd9db65
Victor Aynbinder <>
2023-05-07 21:19:21 +0300

    Adding test file for ec2. Need to delete it after

--- career_proj/prod_settings.py
+++ career_proj/prod_settings.py
@@ -183,6 +183,8 @@ COUNTRIES_FIRST = [ 'US', 'IL' ]
 #     ('* * * * *', 'jobs_app.run_scraper.run_linkedin_scrap_process','>> /tmp/linkedin_scraper.log 2>&1')
 # ]
 
+
+CRONTAB_DJANGO_SETTINGS_MODULE = 'career_proj.prod_settings.py'
 CRONJOBS = [
     # ('* * * * *', 'myapp.cron.my_scheduled_job')
     # ('* * * * *', 'jobs_app.run_scraper.run_linkedin_scrap_process')
