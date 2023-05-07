0aca4b5f76d7db52eafbf523495ec095ce9826b2
Victor Aynbinder <>
2023-05-07 21:45:55 +0300

    Adding test file for ec2. Need to delete it after

--- jobs_app/run_scraper.py
+++ jobs_app/run_scraper.py
@@ -3,3 +3,4 @@ import subprocess
 # def run_linkedin_scrap_process():
 #     subprocess.run(['python3','src/career/jobs_scrappers/linkedin_scrapper/get_linkedin_jobs_to_db.py'])
 print("Instead of running scrapping")
+subprocess.run(['python3','/home/ubuntu/src/career/jobs_scrappers/linkedin_scrapper/get_linkedin_jobs_to_db.py'])
