26fe1b9d25353aa18217a3327cd8d7a4f24a7c21
Victor Aynbinder <>
2023-05-07 19:11:51 +0300

    Adding test file for ec2. Need to delete it after

--- run_scraper.py
+++ run_scraper.py
@@ -1,4 +1,4 @@
 import subprocess
 
 # def run_linkedin_scrap_process():
-subprocess.run(['cd /home/ubuntu/src/career && venv/bin/python3','jobs_scrappers/linkedin_scrapper/get_linkedin_jobs_to_db.py'])
+subprocess.run(['/home/ubuntu/src/career && venv/bin/python3','jobs_scrappers/linkedin_scrapper/get_linkedin_jobs_to_db.py'])
