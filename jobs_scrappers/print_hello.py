f382fb4b123d2e3a4007d5b3bcc42cb173d7ddc9
Victor Aynbinder <>
2023-05-07 15:22:44 +0300

    Adding test file for ec2. Need to delete it after

--- jobs_scrappers/print_hello.py
+++ jobs_scrappers/print_hello.py
@@ -1,7 +1,16 @@
 import json
 
+from jobs_scrappers.linkedin_scrapper.LinkedInToDB import LinkedInToDB
+
 print("Hello from (print_hello)")
 with open('linkedin_scrapper/scraped_files/israel_test_4_noTitle-2023-05-03.json', 'r') as fh:
     all_file = json.load(fh)
 
 print(f"File length  = {len(all_file)}")
+
+for jset in all_file:
+    print(f"Inside loop over all_file")
+    db_transporter = LinkedInToDB(dict_25_jobs=jset)
+    print(f"After db_transporter = LinkedInToDB(dict_25_jobs=jset)")
+    # db_transporter.linkedin_dict_to_db_companies()
+    # db_transporter.linkedin_dict_to_db_jobs()
